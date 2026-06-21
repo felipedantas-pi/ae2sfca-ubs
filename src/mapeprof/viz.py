6b96bf76de192058b8418f34d486734c5a8239e0
felipedantas-pi <tecnologo.felipedantas@gmail.com>
2026-06-21 17:40:45 -0300

    feat(viz): helpers formatar_eixos_mapa e ajustar_zoom_camada (padrao A5)

--- src/mapeprof/viz.py
+++ src/mapeprof/viz.py
@@ -23,7 +23,7 @@ def aplicar_estilo():
         'xtick.labelsize': 8, 'ytick.labelsize': 8,
         'figure.facecolor': 'white', 'axes.facecolor': '#F8F8F8',
         'axes.grid': True, 'grid.alpha': 0.4, 'grid.linestyle': '--',
-        'legend.fontsize': 6, 'legend.title_fontsize': 8, 'legend.labelspacing': 1.2,
+        'legend.fontsize': 5, 'legend.title_fontsize': 7, 'legend.labelspacing': 1.2,
         'legend.handletextpad': 0.8, 'legend.borderpad': 0.8
     })
 
